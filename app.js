3f9d0c6a1a3f25c898a7bf1138dafe0b63ec8bee
xuyingjue <1283638850@qq.com>
2025-07-11 00:55:47 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+import { createPinia } from 'pinia';
+
+App({
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log('小程序初始化完成');
+    this.pinia = createPinia();
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log('小程序显示');
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log('小程序隐藏');
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.error('小程序错误:', msg);
+  }
+})
